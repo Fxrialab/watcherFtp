090fe3a3dc53bf13df86c13cc41cd0a8ff40aa2b
Minh Tran <gtdminh@gmail.com>
2014-03-12 19:04:31 +0700

    add maven assembly and shade for jar2exe

---
+++ src/main/java/fxrialab/utils/watcherFtp/operations/WatchThread.java
@@ -0,0 +1,35 @@
+package fxrialab.utils.watcherFtp.operations;
+
+import fxrialab.utils.watcherFtp.domains.Config;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class WatchThread implements Runnable
+{
+    private Config cfg;
+    public WatchThread(Config cfg)
+    {
+        this.cfg = cfg;
+    }
+
+    @Override
+    public void run()
+    {
+        DirWatcher local;
+        try
+        {
+            local = new DirWatcher(Paths.get(cfg.getLocalFolder()));
+            RemoteDir remote = new RemoteDir(cfg.getHost(),cfg.getPort(),
+                    cfg.getUser(),cfg.getPassword(),
+                    cfg.getRemoteFolder());
+            System.out.println("start watching folder " + cfg.getLocalFolder());
+            remote.listen(local);
+            local.start();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
